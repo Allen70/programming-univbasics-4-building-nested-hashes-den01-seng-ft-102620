811de71719e45c8b8c2861b3fa75d154797278ad
Allen Richard Lea <allenlea2007@gmail.com>
2020-10-08 17:23:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -67,7 +67,9 @@ newhash = {
       b_and_o_railroad: {
                 mortgage_value: 400
       },
-      shortline_railroad:{}
+      shortline_railroad:{
+                mortgage_value: 800
+      }
     }
   }
 }  
