eceeefd02cd826f282386d7d9380aa21dc4a4168
Allen Richard Lea <allenlea2007@gmail.com>
2020-10-08 17:08:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,8 +10,10 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 newhash = {
-  railroads: {},
-  key: 4
+  railroads: {
+    key: 4
+  },
+
 }  
 
 end
